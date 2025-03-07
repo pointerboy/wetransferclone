e40aaf6d1f16f86968fd8e2d76b4be2e7df76ac8
pointerboy <lukatrbovic@outlook.com>
2025-03-07 19:47:51 +0100

    new

--- app.py
+++ app.py
@@ -1961,4 +1961,4 @@ async def download_file(file_id: str, filename: str, background_tasks: Backgroun
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
